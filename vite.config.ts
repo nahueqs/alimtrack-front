77167ee5d10bbd30ebf9197953b142834c40f804
nahue <qsnahuel@gmail.com>
2026-01-25 01:12:23 -0300

    agrego gh pages a package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // base: './', // <--- ELIMINADO: Vercel usa la raíz por defecto
   assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.svg'],
   server: {
     port: 3000,
